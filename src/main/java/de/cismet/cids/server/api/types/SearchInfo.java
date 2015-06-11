74966f3b44b74164cc918f006b6fef5d5a489ac5
Pascal Dihé <pascal@cismet.de>
2015-06-11 17:24:30 +0200

    #7 extended SearchInfo and new Type information for json deserialization

--- src/main/java/de/cismet/cids/server/api/types/SearchInfo.java
+++ src/main/java/de/cismet/cids/server/api/types/SearchInfo.java
@@ -7,18 +7,20 @@
 ****************************************************/
 package de.cismet.cids.server.api.types;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Map;
+import java.util.List;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 import de.cismet.cids.base.types.Key;
 
 /**
- * DOCUMENT ME!
+ * Provides information on server searches.
  *
  * @author   thorsten
  * @version  1.0
@@ -34,5 +36,29 @@ public class SearchInfo implements Key {
     private String key;
     private String name;
     private String description;
-    private Map<String, String> parameterDescription;
+    private List<SearchParameterInfo> parameterDescription;
+    /** Description of the return type of the search. */
+    private SearchParameterInfo resultDescription;
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * Gets the SearchParameterInfo of the specified parameter (key).
+     *
+     * @param   key  name of the parameter
+     *
+     * @return  SearchParameterInfo or null
+     */
+    @JsonIgnore
+    public SearchParameterInfo getSearchParameterInfo(final String key) {
+        if ((this.parameterDescription != null) && !this.parameterDescription.isEmpty()) {
+            for (final SearchParameterInfo searchParameterInfo : this.parameterDescription) {
+                if (searchParameterInfo.getKey().equalsIgnoreCase(key)) {
+                    return searchParameterInfo;
+                }
+            }
+        }
+
+        return null;
+    }
 }
---
+++ src/main/java/de/cismet/cids/server/api/types/SearchParameterInfo.java
@@ -0,0 +1,53 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.server.api.types;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import de.cismet.cids.base.types.Key;
+import de.cismet.cids.base.types.Type;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+/**
+ * Provides name and Type information about an Search Parameter.
+ *
+ * @author   Pascal Dihé
+ * @version  $Revision$, $Date$
+ */
+@XmlRootElement
+@Data
+@NoArgsConstructor
+@JsonInclude(Include.NON_DEFAULT)
+public class SearchParameterInfo implements Key {
+
+    //~ Instance fields --------------------------------------------------------
+
+    /** Name of the Parameter. */
+    private String key;
+
+    /** Type op the Parameter. */
+    private Type type;
+
+    /** Additional info on a type, e.g. Java Class of a JAVA_TYPE */
+    private String additionalTypeInfo;
+
+    /** Determines if the Type is an array Type. */
+    private boolean isArray = false;
+}
