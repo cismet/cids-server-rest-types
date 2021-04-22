bc70cb57c80f96afbed4796548a18b0ed3e82d33
therter <thorsten.herter@cismet.de>
2021-04-22 09:49:26 +0200

    #42 fill the payload properly (it must be divisible by 4)

--- src/main/java/de/cismet/cidsx/server/api/types/User.java
+++ src/main/java/de/cismet/cidsx/server/api/types/User.java
@@ -1,18 +1,16 @@
-/** *************************************************
- *
- * cismet GmbH, Saarbruecken, Germany
- *
- *              ... and it just works.
- *
- *************************************************** */
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 package de.cismet.cidsx.server.api.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.sun.jersey.core.util.Base64;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,6 +20,10 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 import org.mindrot.jbcrypt.BCrypt;
+
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,43 +35,41 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * DOCUMENT ME!
  *
- * @author thorsten
- * @version 1.0
+ * @author   thorsten
+ * @version  1.0
  */
 @XmlRootElement
 @Data
 @Slf4j
-@EqualsAndHashCode(exclude = {"jwt", "pass", "passHash", "userGroups", "validated"})
+@EqualsAndHashCode(exclude = { "jwt", "pass", "passHash", "userGroups", "validated" })
 @NoArgsConstructor
 public class User {
 
     //~ Static fields/initializers ---------------------------------------------
+
     public static final User NONE = makeNONE();
     public static final String BASIC_AUTH_PREFIX = "Basic ";
     public static final String BEARER_AUTH_PREFIX = "Bearer ";
 
     //~ Instance fields --------------------------------------------------------
-    @NonNull
-    private String user;
+
+    @NonNull private String user;
     private String domain = "local"; // NOI18N;
     private String jwt;
     private String passHash;
-    @NonNull
-    private Collection<String> userGroups = new ArrayList<String>();
+    @NonNull private Collection<String> userGroups = new ArrayList<String>();
 
-    @JsonIgnore
-    private String pass;
-    @JsonIgnore
-    private boolean validated;
+    @JsonIgnore private String pass;
+    @JsonIgnore private boolean validated;
 
     //~ Constructors -----------------------------------------------------------
+
     /**
-     * Creates a new User object from an authString of format
-     * 'username[@domain]'.
+     * Creates a new User object from an authString of format 'username[@domain]'.
      *
-     * @param authString  <code>String</code> of format 'username[@domain]'
+     * @param   authString  <code>String</code> of format 'username[@domain]'
      *
-     * @throws IllegalArgumentException DOCUMENT ME!
+     * @throws  IllegalArgumentException  DOCUMENT ME!
      */
     public User(@NonNull final String authString) {
         if (authString.startsWith(BASIC_AUTH_PREFIX)) {
@@ -98,14 +98,16 @@ public class User {
             jwt = authString.substring(BEARER_AUTH_PREFIX.length());
             try {
                 jwt = authString.substring(BEARER_AUTH_PREFIX.length());
-                String b64Payload = jwt.split("\\.")[1];
+                final String b64Payload = jwt.split("\\.")[1];
                 String paddedPayload = b64Payload;
-                for (int i = 0; i < b64Payload.length() % 4; ++i) {
+                
+                for (int i = 0; i < ((4 - (b64Payload.length() % 4)) % 4); ++i) {
                     paddedPayload = paddedPayload + "=";
-
                 }
-                String encodedString = new String(Base64.decode((paddedPayload).getBytes(StandardCharsets.UTF_8)));
-                Map map = new ObjectMapper().readValue(encodedString, Map.class);
+                
+                final String encodedString = new String(Base64.decode(
+                            (paddedPayload).getBytes(StandardCharsets.UTF_8)));
+                final Map map = new ObjectMapper().readValue(encodedString, Map.class);
                 user = map.get("sub").toString();
             } catch (IOException ex) {
                 log.warn("Problem during extraction of username from jwt Token", ex);
@@ -119,10 +121,10 @@ public class User {
     /**
      * Creates a new User object.
      *
-     * @param user DOCUMENT ME!
-     * @param domain DOCUMENT ME!
-     * @param pass DOCUMENT ME!
-     * @param groups DOCUMENT ME!
+     * @param  user    DOCUMENT ME!
+     * @param  domain  DOCUMENT ME!
+     * @param  pass    DOCUMENT ME!
+     * @param  groups  DOCUMENT ME!
      */
     public User(final String user, final String domain, final String pass, final String... groups) {
         validated = false;
@@ -134,10 +136,11 @@ public class User {
     }
 
     //~ Methods ----------------------------------------------------------------
+
     /**
      * DOCUMENT ME!
      *
-     * @return DOCUMENT ME!
+     * @return  DOCUMENT ME!
      */
     private static User makeNONE() {
         final User u = new User(null, null, null);
